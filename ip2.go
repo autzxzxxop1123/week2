9160e561509b235aa1f424219d74175b1fe6bf11
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2019-11-21 16:02:39 +0700

    test2_week2

---
+++ ip2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	//
+	fmt.Printf("10 (base 2 ) = %b \n", 10 ) 
+	fmt.Printf("10 (base 8 ) = %o \n", 10 )
+	fmt.Printf("10 (base 10 ) = %d \n",10 )
+	fmt.Printf("10 (base 16 ) = %x \n",10 )
+
+}
