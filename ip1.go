836b9512519a1212469e4ccfbccbdceb02bfd4e0
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2019-11-21 16:02:07 +0700

    test1_week2

---
+++ ip1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("My name is %s, I am %d year old\n", "aut", 21)
+	fmt.Printf("Pi:%E(%T) \n",3.14159265359,30)
+	fmt.Printf("Pi:%E(%T) \n",3.14159265359,3.01111111111111111111)
+	fmt.Printf("1>2: %t \n",2>1)
+	fmt.Printf("Pi:%s \n","\"aut\"")
+	fmt.Printf("Pi:%q \n","\"aut\"")
+	fmt.Printf("Pi:%b \n",20)
+	fmt.Printf("Pi:%d \n",20)
+	fmt.Printf("Pi:%o \n",20)
+	fmt.Printf("Pi:%x \n",10)
+	fmt.Printf("Pi:%X \n",10)
+	fmt.Printf("Pi:%c \n",35)
+	fmt.Printf("Pi:%q \n",35)
+	fmt.Printf("Pi:%g \n",3)
+
+
+
+
+}
